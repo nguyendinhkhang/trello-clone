36bcfb5f3f4c747f9389ce87baa99d6b3945c035
nguyendinhkhang <khangteoem@gmail.com>
2022-02-12 16:14:18 +0700

    UI

---
+++ client/src/App.jsx
@@ -0,0 +1,80 @@
+import './App.scss'
+
+function App() {
+  return (
+    <div className="App">
+      <nav className='navbar app'>App Bar</nav>
+      <nav className='navbar dashboard'>Dashboard Bar</nav>
+      <div className="dashboard-content">
+        <div className='dashboard-column'>
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src='https://i.pinimg.com/736x/ae/38/7b/ae387bfcea634b7b9818d25f3d637f3b.jpg' alt='Brainstorm' />
+              Title: Tatto
+            </li>
+            <li>Add what do you'd like to work on bellow</li>
+            <li>Add what do you'd like to work on bellow</li>
+            <li>Add what do you'd like to work on bellow</li>
+            <li>Add what do you'd like to work on bellow</li>
+            <li>Add what do you'd like to work on bellow</li>
+            <li>Add what do you'd like to work on bellow</li>
+            <li>Add what do you'd like to work on bellow</li>
+            <li>Add what do you'd like to work on bellow</li>
+            <li>Add what do you'd like to work on bellow</li>
+            <li>Add what do you'd like to work on bellow</li>
+            <li>Add what do you'd like to work on bellow</li>
+            <li>Add what do you'd like to work on bellow</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className='dashboard-column'>
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src='https://i.pinimg.com/736x/ae/38/7b/ae387bfcea634b7b9818d25f3d637f3b.jpg' alt='Brainstorm' />
+              Title: Tatto
+            </li>
+            <li>Add what do you'd like to work on bellow</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className='dashboard-column'>
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src='https://i.pinimg.com/736x/ae/38/7b/ae387bfcea634b7b9818d25f3d637f3b.jpg' alt='Brainstorm' />
+              Title: Tatto
+            </li>
+            <li>Add what do you'd like to work on bellow</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className='dashboard-column'>
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src='https://i.pinimg.com/736x/ae/38/7b/ae387bfcea634b7b9818d25f3d637f3b.jpg' alt='Brainstorm' />
+              Title: Tatto
+            </li>
+            <li>Add what do you'd like to work on bellow</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className='dashboard-column'>
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src='https://i.pinimg.com/736x/ae/38/7b/ae387bfcea634b7b9818d25f3d637f3b.jpg' alt='Brainstorm' />
+              Title: Tatto
+            </li>
+            <li>Add what do you'd like to work on bellow</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
